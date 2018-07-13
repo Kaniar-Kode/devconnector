54f4323abe327b9e53b180ad8771a2aeb45e8870
Kaniar-Kode <noel.christophe.11@gmail.com>
2018-07-13 21:51:41 +0400

    JWT Auth Modification

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://totof:zamal974@ds117200.mlab.com:17200/kaniarbiz'
+    mongoURI: 'mongodb://totof:zamal974@ds117200.mlab.com:17200/kaniarbiz',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const posts = require('./routes/api/posts')
 const profile = require('./routes/api/profile')
 const users = require('./routes/api/users')
 
 const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // DB Config
 const db = require('./config/keys').mongoURI
@@ -20,10 +26,11 @@ mongoose
         console.log(err)
     })
 
-app.get('/', (req, res) => {
-    res.send('Hello king of cannabis !')
-})
+// Passport middleware
+app.use(passport.initialize())
 
+// Passport config
+require('./config/passport')(passport)
 
 // Use routes
 app.use('/api/posts', posts)
